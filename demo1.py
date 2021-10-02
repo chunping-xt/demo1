557a3238393edabdea8b6b093a7764338b13492e
chunping-xt <87054579+chunping-xt@users.noreply.github.com>
2021-10-02 09:32:27 +0700

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,7 +1,1 @@
-echo "# demo1" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/chunping-xt/demo1.git
-git push -u origin main
+
